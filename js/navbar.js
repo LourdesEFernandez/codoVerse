b5d9a0e8ad9acda092cac57c6278b964c6044790
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2024-04-04 12:17:26 -0300

    agregue el difuminado al footer tambien

--- js/navbar.js
+++ js/navbar.js
@@ -3,14 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.querySelector(".sidebar");
   const closeBtn = document.querySelector(".close-btn");
   const mainContent = document.querySelector(".main");
+  const footer = document.querySelector("#footer");
 
   hamburgerMenu.addEventListener("click", function () {
     sidebar.classList.toggle("open");
     mainContent.classList.toggle("blur"); // Agregamos o quitamos la clase 'blur'
+    footer.classList.toggle("blur");
   });
 
   closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("open");
     mainContent.classList.remove("blur"); // Quitamos la clase 'blur' al cerrar el menú
+    footer.classList.remove("blur"); // Quitamos la clase 'blur' al cerrar el menú
   });
 });
