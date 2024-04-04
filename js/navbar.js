2fc50c5548574d698ac635a3667caf78df576678
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2024-04-04 11:48:40 -0300

    agregue variables generales en el css del navbar y solucione error de difuminado al abrir el menu de hamburguesa en dispositivo responsive

--- js/navbar.js
+++ js/navbar.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenu = document.querySelector(".hamburger-menu");
   const sidebar = document.querySelector(".sidebar");
   const closeBtn = document.querySelector(".close-btn");
+  const mainContent = document.querySelector(".main");
 
   hamburgerMenu.addEventListener("click", function () {
     sidebar.classList.toggle("open");
+    mainContent.classList.toggle("blur"); // Agregamos o quitamos la clase 'blur'
   });
 
   closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("open");
+    mainContent.classList.remove("blur"); // Quitamos la clase 'blur' al cerrar el menú
   });
 });
