945c4598c7e144bc19a85c759cca4e43760a5e5d
LourdesEFernandez <139290318+LourdesEFernandez@users.noreply.github.com>
2024-05-02 02:34:52 -0300

    Corregí direccion de imagenes y gradiente de background en index.

--- js/info_main.js
+++ js/info_main.js
@@ -55,53 +55,90 @@ const switchCard = () =>{
 // lista de juegos 
 // atributos de un juego nombre, info principal, info complementaria, id, imagen,precio,descuento,stock,genero,tipo de plataforma
 const listGames = [
+    {name:"Resident Evil 3 Remake", 
+    code: "game",
+    img:"../assets/images/game_resident_evil_3.jpeg",
+    description:"Resident Evil 3 Remake es la reinterpretación del clásico de Capcom, Resident Evil 3: Nemesis, la tercera parte de la trilogía original de survival horror. Siguiendo con las pasos de la segunda parte, y con un aspecto jugable y gráfico renovado en base a las actuales plataformas, nos contará la historia de Jill Valentine y su huida de la ciudad de Raccoon City infestada por el virus T. Incluirá el modo multijugador Resident Evil Resistance. ",
+    price:"59,999",
+    infoTitle:"Ficha técnica de la versión PS4",
+    date:"3/4/2020",
+    players:"1",
+    format:"Blu-ray",
+    texts:"Español",
+    voices:"Español",
+    online:"Si",
+    requirements:"PC"
+    },
+    {name:"Red Dead Redemption 2", 
+    code: "game",
+    img:"../assets/images/game_red_dead_redemption_2.jpeg",
+    description:"Red Dead Redemption 2 es la secuela del videojuego de éxito de Rockstar Red Dead Redemption (2010), considerado como uno de los mejores títulos de la compañía. Se trata de la tercera entrega de la saga Red Dead, iniciada en 2004 con Red Dead Revolver y nos trasladará de nuevo al Salvaje Oeste para ponernos en el escenario de mundo abierto más grande jamás creado por los padres de Grand Theft Auto y contarnos una historia sobre forajidos, venganza y atracos en un escenario de gran realismo en PlayStation 4 y Xbox One. ",
+    price:"69,999",
+    infoTitle:"Ficha técnica de la versión PS4",
+    date:"26/10/2018",
+    players:"1",
+    format:"Blu-ray",
+    texts:"Español",
+    voices:"Inglés",
+    online:"Si",
+    requirements:"PC"
+    },
+    
     {name:"Final Fantasy VII Remake", 
     code: "game",
     img:"../assets/images/game_final_fantasy_VII_remake.jpg",description:"Nueva adaptación de la obra maestra del rol japonés. El remake del séptimo capítulo de la saga nos trasladará al mundo de la entrega original de PlayStation y PC, renovando sus gráficos para la consola PlayStation 4, añadiendo nuevos detalles la historia, así como aportando presumibles cambios jugables al sistema de batallas y de exploración. Este nuevo 'Final Fantasy VII' tiene detrás al mismo equipo creativo del original.",
     price:"50,000",
-    info:{
-        title:"Ficha técnica de la versión PS4",date:"10/4/2020",players:"1",format:"Blu-ray",texts:"Español",voices:"Inglés/Japonés",online:"No",requirements:"-"
-    }},
+    infoTitle:"Ficha técnica de la versión PS4",
+    date:"10/4/2020",
+    players:"1",
+    format:"Blu-ray",
+    texts:"Español",
+    voices:"Inglés/Japonés",
+    online:"No",
+    requirements:"-"
+    },
+
     {name:"Helldivers 2 ", 
     code:"game",
     img:"../assets/images/game_helldivers_2.jpg",
     description:"Es un shooter en tercera persona desarrollado por Arrowhead Game Studios y publicado por Sony Interactive Entertainment para PlayStation 5 y PC. El divertido shooter de doble stick Helldivers vuelve con una ambiciosa secuela que se pasa a una perspectiva en tercera persona, para ofrecer una intensa aventura de acción cooperativa en la que combatimos contra cientos de alienígenas. ",
     price:"39,999",
-    info:[
-        {title:"Ficha técnica de la versión PS5",date:"8/2/2024",players:"1-4",format:"Descarga",texts:"Español",voices:"Español",online:"Si",requirements:"PC, PS5"}
-    ]},
+    infoTitle:"Ficha técnica de la versión PS5",
+    date:"8/2/2024",
+    players:"1-4",
+    format:"Descarga",
+    texts:"Español",
+    voices:"Español",
+    online:"Si",
+    requirements:"PC, PS5"},
+
     {name:"The Legend of Zelda: Tears of the Kingdom", 
     code:"game",
     img:"../assets/images/game_the_legend_of_zelda.jpg",
     description:"Es el nuevo videojuego para Nintendo Switch de la saga de juegos de The Legend of Zelda, secuela directa de Breath of the Wild. Protagonizada por Link, y con una jugabilidad basada en la acción y el rol en mundo abierto, esta aventura nos devuelve a Hyrule con una búsqueda de la Princesa Zelda que cuenta con mecánicas de fusión de objetos y armas más avanzadas, nuevas zonas por explorar en los cielos y mucho más.",
     price:"59,999",
-    info:[
-        {title:"Ficha técnica de la versión Switch",date:"12/5/2023",players:"1",format:"Tarjeta",texts:"Español",voices:"Español",online:"-",requirements:"-"}
-    ]},
-    {name:"Grand Theft Auto V", 
-    code:"game",
-    img:"../assets/images/game_grand_theft_auto_v.jpeg",
-    description:"Grand Theft Auto V para PS4 y Xbox One es una versión mejorada y ampliada del videojuego de acción en mundo abierto Grand Theft Auto V desarrollado por Rockstar lanzado en 2013. Esta versión del éxito, más ambiciosa técnicamente, presenta gráficos y nuevo contenido como armas, vehículos o misiones, además de una banda sonora ampliada y más jugadores en el modo online. Vuelve a presentarnos la historia de Michael, Franklin y Trevor en la ciudad de Los Santos y sus alrededores, una de las más celebradas de la historia del videojuego. ",
-    price:"72,500",
-    info:[
-        {title:"Ficha técnica de la versión PS4",date:"18/11/2014",players:"1",format:"Blu-ray",texts:"Español",voices:"Inglés",online:"Si",requirements:"PS4"}
-    ]},
+    infoTitle:"Ficha técnica de la versión Switch",
+    date:"12/5/2023",
+    players:"1",
+    format:"Tarjeta",
+    texts:"Español",
+    voices:"Español",
+    online:"-",
+    requirements:"-"},
+
     {name:"Elden Ring", 
     code:"game",
     img:"../assets/images/game_elden_ring.jpg",
     description:"Es el nuevo videojuego de FromSoftware, creadores de Dark Souls, Sekiro o Bloodborne. Se trata del nuevo título de acción y rol para un jugador ideado por Hidetaka Miyazaki, que en esta ocasión, estrenará un mundo abierto más grande y ambicioso. Su argumento y mitología están firmados por George R.R. Martin, autor de Canción de hielo y fuego. ",
     price:"69,999",
-    info:[
-        {title:"Ficha técnica de la versión PS4",date:"25/2/2022",players:"1",format:"Blu-ray",texts:"Español",voices:"Inglés",online:"Si",requirements:"PS4"}
-    ]},
+    infoTitle:"Ficha técnica de la versión PS4",date:"25/2/2022",players:"1",format:"Blu-ray",texts:"Español",voices:"Inglés",online:"Si",requirements:"PS4"},
+
     {name:"Assassin's Creed Valhalla", 
     code: "game",
     img:"../assets/images/game_assassin's_creed_valhalla.jpeg",
     description:"Es la nueva entrega de Assassin’s Creed de Ubisoft, la saga de acción y aventuras en mundo abierto más famosa de la desarrolladora de videojuegos. En esta ocasión, y siguiendo con los toques RPG de las últimas entregas, viajaremos al siglo IX después de Cristo, llegando a una Gran Bretaña invadida por vikingos. Está disponible para Xbox One, Xbox Series S/S, PlayStation 4, PlayStation 5 y PC. ",
     price:"59,999",
-    info:[
-        {title:"Ficha técnica de la versión PC",date:"6/12/2022",players:"1",format:"Descarga",texts:"Español",voices:"Español",online:"-",requirements:"PC"}
-    ]},
+    infoTitle:"Ficha técnica de la versión PC",date:"6/12/2022",players:"1",format:"Descarga",texts:"Español",voices:"Español",online:"-",requirements:"PC"},
 ]
 
 // mi contenedor de juego tiene tres partes, imagen y precio, informacion del juego y botones favorito, agregar a carrito y comprar. Crear cada parte por separado y luego insertar todo en un solo contenedor.
@@ -129,15 +166,15 @@ const createListGames = () => {
                     <p id="info-game-i-m">${game.description}</p>
                     <button id="show-info-main" class="btn-info">... Ver más</button>
                     <div id="info-2">
-                        <p id="subtitle-i-m">${game.info.title}</p>
+                        <p id="subtitle-i-m">${game.infoTitle}</p>
                         <ul id="data-sheet-i-m">
-                        <li>Fecha de lanzamiento: ${game.info.date}</li>
-                        <li>Jugadores: ${game.info.players}</li>
-                        <li>Formato: ${game.info.format}</li>
-                        <li>Textos: ${game.info.texts}</li>
-                        <li>Voces: ${game.info.voices}</li>
-                        <li>Online: ${game.info.online}</li>
-                        <li>Requisitos: ${game.info.requirements}</li>
+                        <li>Fecha de lanzamiento: ${game.date}</li>
+                        <li>Jugadores: ${game.players}</li>
+                        <li>Formato: ${game.format}</li>
+                        <li>Textos: ${game.texts}</li>
+                        <li>Voces: ${game.voices}</li>
+                        <li>Online: ${game.online}</li>
+                        <li>Requisitos: ${game.requirements}</li>
                         </ul>
                         <button id="hidden-info-main" class="btn-info" type="button">Ver menos</button>
                     </div>
